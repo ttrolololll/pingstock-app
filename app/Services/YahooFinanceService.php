8e6f07175ba7c1f65a28abb7a2a4a7c7ea3d8c48
Jonathan Yang <jonathan.yang@grabtaxi.com>
2020-01-22 13:56:49 +0800

    yahoo service chart

--- app/Services/YahooFinanceService.php
+++ app/Services/YahooFinanceService.php
@@ -21,6 +21,14 @@ class YahooFinanceService
         ]);
     }
 
+    /**
+     * Returns general information about a stock symbol
+     *
+     * @param $symbol
+     * @param bool $isAsync
+     * @param array $options
+     * @return \GuzzleHttp\Promise\PromiseInterface|\Psr\Http\Message\ResponseInterface
+     */
     public function quoteType($symbol, $isAsync = false, $options = [])
     {
         $opt = [];
@@ -36,6 +44,39 @@ class YahooFinanceService
         }
 
         return $this->http->get($this->baseUrl . '/v1/finance/quoteType/' . $symbol, [
+            'query' => $opt
+        ]);
+    }
+
+    /**
+     * Returns additional metadata and chart values for a given symbol
+     *
+     * @param $symbol
+     * @param bool $isAsync
+     * @param array $options
+     * @return \GuzzleHttp\Promise\PromiseInterface|\Psr\Http\Message\ResponseInterface
+     */
+    public function chart($symbol, $isAsync = false, $options = [])
+    {
+        $opt = [];
+
+        if (is_array($options) && count($options) > 0) {
+            $opt = $options;
+        }
+
+        $opt['region'] = 'SG';
+        $opt['lang'] = 'en-SG';
+        $opt['includePrePost'] = false;
+        $opt['interval'] = '2m';
+        $opt['range'] = '1d';
+
+        if ($isAsync) {
+            return $this->http->getAsync($this->baseUrl . '/v8/finance/chart/' . $symbol, [
+                'query' => $opt
+            ]);
+        }
+
+        return $this->http->get($this->baseUrl . '/v8/finance/chart/' . $symbol, [
             'query' => $opt
         ]);
     }
