503455c8e670e25646e89e1212a1088e043fc350
Jonathan Yang <jonathan.yxy@outlook.com>
2020-02-28 22:20:55 +0800

    add tntsearch to Stock model

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Stock extends Model
 {
+    use Searchable;
+
     protected $fillable = [
         'symbol',
         'name',
@@ -13,15 +16,26 @@ class Stock extends Model
         'exchange_symbol',
         'timezone'
     ];
-    
-    
+
+
     protected $dates = [
         'created_at',
         'updated_at',
-    
+
     ];
-    
+
     protected $appends = ['resource_url'];
+
+    public function toSearchableArray()
+    {
+        $array = [
+            'id' => $this->id,
+            'symbol' => $this->symbol,
+            'name' => $this->name
+        ];
+
+        return $array;
+    }
 
     public function getResourceUrlAttribute()
     {
